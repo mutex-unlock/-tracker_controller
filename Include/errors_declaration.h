ab0e12b75e95ae46d1e711219d4a3bfc323fb8d8
mutex-unlock <mutex-unlock@users.noreply.github.com>
2026-06-25 13:05:13 +0300

    feat(core): add declarations for generating exceptions

---
+++ Include/errors_declaration.h
@@ -0,0 +1,42 @@
+#pragma once
+// исключения для обработки ошибок
+
+
+#include <exception>
+#include <cstdint>
+
+namespace NTracker
+{
+    // перечисление с кодами возможных ошибок
+    enum class ETrackerError: uint32_t
+    {
+        NoError               = 0,
+        NotInitialized        = 1,
+        UnknownTrackerType    = 2,
+        UnknownTrackerNumber  = 3,
+        TrackerIsNotOperable  = 4,
+        DriverIsNotSet        = 5,
+        InvalidArgument       = 6,
+        NotSupportedOperation = 7,
+        InitDriverError       = 8
+    };
+
+    // объявление класса исключений
+    class TTrackerException : public std::exception
+    {
+    public:
+        TTrackerException(ETrackerError error);
+        
+        ETrackerError code() const noexcept;
+        const char* what() const noexcept override;
+
+        // если при выполнении где-то в коде возникает ошибка,
+        // то в этом месте нужно вызвать метод throw_exception()
+        // Указанный метод выбросит исключение с соответствующим кодом.
+        static void throw_exception(ETrackerError error);
+
+    private:
+        ETrackerError code_;        // код ошибки
+    };
+
+};  // end namespace NTracker

46c1d2eb132535c89d7bc3bf20ec5de33dc905b6
mutex-unlock <mutex-unlock@users.noreply.github.com>
2026-06-26 17:48:29 +0300

    feat(core): add implementation of the TRequestQueue class interface for making asynchronous callbacks

---
+++ Source/request_queue.cpp
@@ -0,0 +1,80 @@
+#include "request_queue.h"
+#include "tracker_interface.h"
+// реализация интерфейса класса для выполнения асинхронных вызовов callbacks.
+
+// Для реализации асинхронных запросов объявляется очередь,
+// в которую помещаются все поступающие запросы.
+// Обработка очереди происходит в отдельном потоке.
+// Поток извлекает очередной запрос и для него выполняет обратный вызов
+
+using namespace NTracker;
+
+
+// метод, который запускает поток обработки очереди
+void TRequestQueue::start_queue()
+{
+    if (!queueThread_.joinable())
+    {
+        queueThread_ = std::jthread([this](std::stop_token stoken) {
+            this->read_request(stoken);
+        });
+    }
+}
+
+
+// метод для остановки потока обработки очереди
+void TRequestQueue::stop_queue()
+{
+    if (queueThread_.joinable())
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        queueThread_.request_stop();        // C++20: Запрос остановки потока
+
+        lock.unlock();
+        conditional_.notify_one();
+        queueThread_.join();
+        
+        requestQueue_ = decltype(requestQueue_)();
+    }
+}
+
+
+// добавляет переданные данные в очередь
+// путем создания экземпляра структуры Request
+// и размещения ее в очереди requestQueue_
+void TRequestQueue::add_request(trackerNumber_t trackerNumber,
+                                trackerPointer_t trackerPointer,
+                                trackerValueCallback_t callback)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    requestQueue_.push({ trackerNumber, trackerPointer, callback });
+
+    conditional_.notify_one();
+}
+
+
+// Обработка очереди реализована в методе read_request().
+// Поток обработки очереди вызывает этот метод,
+// который, в свою очередь, ожидает поступления записей и обрабатывает их
+void TRequestQueue::read_request(std::stop_token stoken)
+{
+    while (!stoken.stop_requested())    // C++20: пока не запрошена остановка потока
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+
+        conditional_.wait(lock, [this, &stoken]()
+        {
+            return requestQueue_.size() > 0 || stoken.stop_requested();
+        });
+
+        while (requestQueue_.size() > 0 && !stoken.stop_requested())
+        {
+            TRequest cmd = requestQueue_.front();
+            requestQueue_.pop();
+            lock.unlock();
+            cmd.callback_(cmd.trackerNumber_, cmd.trackerPointer_->get_current_value());
+            lock.lock();
+        }
+    }
+}

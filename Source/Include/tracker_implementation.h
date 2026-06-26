6305b011219e9e94fef9efb7e200faf05a3a04df
mutex-unlock <mutex-unlock@users.noreply.github.com>
2026-06-26 12:56:36 +0300

    feat(core): add interfaces for concrete tracker classes

---
+++ Source/Include/tracker_implementation.h
@@ -0,0 +1,71 @@
+#pragma once
+// интерфейсы конкретных классов датчиков
+
+#include "tracker_interface.h"
+
+
+namespace NTracker
+{
+
+    class TTrackerAbstract : public ITracker
+    {
+    public:
+        // метод для настройки драйвера, с которым будет работать датчик
+        void set_driver(driverPointer_t driverPtr) override;
+        // получить используемый драйвер
+        driverPointer_t get_driver() override;
+        // метод для определения работоспособности датчика
+        bool is_operable_tracker() override;
+
+    protected:
+        TTrackerAbstract(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+        trackerNumber_t number;
+        driverPointer_t driverPointer;
+    };
+
+
+    // class TTrackerSpot
+    class TTrackerSpot : public TTrackerAbstract
+    {
+    public:
+        // метод для получения текущего значения датчика
+        double get_current_value() override;
+
+        // метод для создания экземпляра класса типа TTrackerSpot
+        static ITracker* create_tracker(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+
+    protected:
+        TTrackerSpot(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+    };
+
+
+    // class TTrackerSmooth
+    class TTrackerSmooth : public TTrackerAbstract
+    {
+    public:
+        // метод для получения текущего значения датчика
+        double get_current_value() override;
+
+        // метод для создания экземпляра класса типа TTrackerSmooth
+        static ITracker* create_tracker(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+
+    protected:
+        TTrackerSmooth(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+    };
+
+
+    // class TTrackerDerivative
+    class TTrackerDerivative : public TTrackerAbstract
+    {
+    public:
+        // метод для получения текущего значения датчика
+        double get_current_value() override;
+
+        // метод для создания экземпляра класса типа TTrackerDerivative
+        static ITracker* create_tracker(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+
+    protected:
+        TTrackerDerivative(trackerNumber_t assignedNumber, driverPointer_t driverPointer);
+    };
+
+};  // end namespace NTracker
